7673673e7bdb05057df37d282f0ccd58e6169a52
rlee287 <rlee287@yahoo.com>
2021-06-25 19:20:36 -0700

    Small fix to bail out upon filename extraction lineread error
    (cherry picked from commit cee32cc06f1a3da30a90f0ae9f2f0084e9e4af57)

--- merkle_tree_checksum/src/main.rs
+++ merkle_tree_checksum/src/main.rs
@@ -267,15 +267,15 @@ fn run() -> i32 {
             };
             loop {
                 let next_line_result = parse_functions::next_noncomment_line(&mut hash_file_reader);
-                if let Err(read_result) = next_line_result {
-                    if read_result.kind() == std::io::ErrorKind::UnexpectedEof {
+                if let Err(read_err) = next_line_result {
+                    if read_err.kind() == std::io::ErrorKind::UnexpectedEof {
                         if !is_short_hash {
                             eprintln!("Error: unexpected EOF reading hashes");
                         }
                     } else {
-                        eprintln!("Error: Error in reading file: {}", read_result);
+                        eprintln!("Error: Error in reading file: {}", read_err);
                     }
-                    break;
+                    return 1;
                 }
                 let next_line = next_line_result.unwrap();
                 if let Some((short_from_regex, quoted_name)) = parse_functions::extract_quoted_filename(&next_line) {
