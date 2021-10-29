f6d0cc1d68360108f667cd8f367aa72a7ee1719d
rlee287 <rlee287@yahoo.com>
2021-10-29 19:39:22 -0400

    Test error response for malformed file
    Both verify_bad tests need more precise checks later

--- merkle_tree_checksum/tests/verify_bad.rs
+++ merkle_tree_checksum/tests/verify_bad.rs
@@ -21,5 +21,26 @@ fn verify_files_short_badhash() {
     assert_check
         .failure()
         .code(2)
+        // TODO: check for "hash mismatch", and that others are fine
         .stderr(predicate::str::contains("Error"));
+}
+
+#[test]
+#[serial]
+fn verify_files_short_malformed() {
+    set_current_dir("tests/reference_files").unwrap();
+    let _guard = scopeguard::guard((), |_| {
+        if current_dir().unwrap().ends_with("tests/reference_files") {
+            set_current_dir("../..").unwrap();
+        }
+    });
+    let mut cmd_check = Command::cargo_bin("merkle_tree_checksum").unwrap();
+    let assert_check = cmd_check
+        .args(["verify-hash", "hash_out_short_badhash"])
+        .assert();
+    assert_check
+        .failure()
+        .code(2)
+        .stderr(predicate::str::contains("Error"));
+    // TODO: check for "malformed entry", and that others are fine
 }
